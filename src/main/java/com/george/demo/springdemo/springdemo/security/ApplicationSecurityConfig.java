088bf869e48c3ade02bb7911843d75b67eb38f54
George Nady <gorgonady.gn@gmail.com>
2022-04-06 10:49:24 +0200

    security

--- src/main/java/com/george/demo/springdemo/springdemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/george/demo/springdemo/springdemo/security/ApplicationSecurityConfig.java
@@ -1,26 +1,62 @@
 package com.george.demo.springdemo.springdemo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.george.demo.springdemo.springdemo.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
 
-                .antMatchers("/","index", "/css/*", "/js/*")
-                .permitAll()
+                // for this patterns ... we will permit all
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+
+                .antMatchers("api/**").hasRole(ADMIN.name())
 
                 .anyRequest()
                 .authenticated()
                 .and()
 
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        // is how you retrieve your users from database
+        UserDetails georgeUserDetails = User.builder()
+                // USER and PASS
+                .username("george")
+                .password(passwordEncoder.encode("01211422185"))
+                // ROLE_USER
+                // TODO: how to use it
+                .roles(ADMIN.name())
+                .build();
+
+        UserDetails josephUserDetails = User.builder()
+                .username("joseph")
+                .password(passwordEncoder.encode("01270680830"))
+                .roles(STUDENT.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(georgeUserDetails, josephUserDetails);
     }
 }
---
+++ src/main/java/com/george/demo/springdemo/springdemo/security/ApplicationUserRole.java
@@ -0,0 +1,18 @@
+package com.george.demo.springdemo.springdemo.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.george.demo.springdemo.springdemo.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(USER_READ, USER_WRITE, COURSE_READ, COURSE_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+}
